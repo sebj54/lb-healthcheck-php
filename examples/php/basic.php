c4cad780feedb8bd203fa37fdfd0d3f35244d645
Sean Bruton <sbruton@hosting.com>
2015-05-26 17:13:09 -0600

    example cleanup

--- examples/php/basic.php
+++ examples/php/basic.php
@@ -3,6 +3,7 @@
 require_once("vendor/autoload.php");
 
 // This example health check function will always succeed
+//
 // Typically you'd put your own test logic here to make sure
 // this server was working as intended
 //
@@ -23,7 +24,7 @@ function myHealthCheck2()
 // You can quickly itterate over all your health check functions
 // by adding them to a key => value array
 // 
-// Array entries can either by the name of your health check function
+// Array entries can either be the name of your health check function
 // as shown for check01 and check02 or an annonymous internal
 // function as shown for check03
 $myHealthChecks = array(

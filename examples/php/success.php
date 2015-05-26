4b39c146b84f4250f159e59ac01da20cff7e23a6
Sean Bruton <sbruton@hosting.com>
2015-05-26 16:40:05 -0600

    adding simple multi-check iterator

--- examples/php/success.php
+++ examples/php/success.php
@@ -2,4 +2,35 @@
 
 require_once("vendor/autoload.php");
 
-\HOSTING\LBHealthCheck::success();
+// This example health check function will always succeed
+// Typically you'd put your own test logic here to make sure
+// this server was working as intended
+function myHealthCheck1()
+{
+	return true;
+}
+
+// This example health check function will always succeed
+// Typically you'd put your own test logic here to make sure
+// this server was working as intended
+function myHealthCheck2()
+{
+	return true;
+}
+
+// You can quickly itterate over all your health check functions
+// by adding them to a key => value array
+// 
+// Array entries can either by the name of your health check function
+// as shown for check01 and check02 or an annonymous internal
+// function as shown for check03
+$myHealthChecks = array(
+		'check01' => 'myHealthCheck1',
+		'check02' => 'myHealthCheck2',
+		'check03' => function() {
+			return true;
+		}
+	);
+
+// Then simply pass the resulting array to \HOSTING\LBHealthCheck::process()
+\HOSTING\LBHealthCheck::process($myHealthChecks);
--- src/main/php/HOSTING/LBHealthCheck.php
+++ src/main/php/HOSTING/LBHealthCheck.php
@@ -17,24 +17,38 @@ class LBHealthCheck
 	const RESULT_FAIL_CODE		= "500";
 	const RESULT_FAIL_MSG		= "Unspecified error";
 
+	public static function process($checks)
+	{
+		$allChecksSuccessful = true;
+		foreach($checks as $check)
+		{
+			if(!call_user_func($check))
+			{
+				$allChecksSuccessful = false;
+			}
+		}
+		if($allChecksSuccessful)
+		{
+			self::success();
+		}
+		else
+		{
+			self::fail();
+		}
+	}
+
 	public static function fail($message = self::RESULT_FAIL_MSG, $data = null)
 	{
-		//$log = LogUtil::initStaticLogger(__CLASS__, __METHOD__, func_get_args());
-		//$log->error("Failure with load balancer health check logic: '".$message."'");
 		echo self::internalResult(self::RESULT_FAIL_CODE, $message, $data);
 	}
 
 	public static function success($message = self::RESULT_SUCCESS_MSG, $data = null)
 	{
-		//$log = LogUtil::initStaticLogger(__CLASS__, __METHOD__, func_get_args());
-		//$log->debug("Success with load balancer health check logic: '".$message."'");
 		echo self::internalResult(self::RESULT_SUCCESS_CODE, $message, $data);
 	}
 
 	private static function internalResult($code, $message, $data)
 	{
-		//$log = LogUtil::initStaticLogger(__CLASS__, __METHOD__, func_get_args());
-		//$log->trace("Handling LB health check result");
 		http_response_code($code);
 		header('Content-Type: application/json');
 		return self::result($code, $message, $data);
@@ -42,8 +56,6 @@ class LBHealthCheck
 
 	public static function result($code, $message, $data)
 	{
-		//$log = LogUtil::initStaticLogger(__CLASS__, __METHOD__, func_get_args());
-		//$log->trace("Generating JSON result message [".$code."]".": ".$message);
 		return RestUtil::result(false, $code, $message, null, $data);
 	}
 }

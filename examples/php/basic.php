706c6e90c00f272b45ed368d4d0f899bf4fc7120
Sébastien JEAN <contact@sebjean.com>
2020-01-15 17:00:39 +0100

    🐛 Fix example

--- examples/php/basic.php
+++ examples/php/basic.php
@@ -9,10 +9,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,7 +22,7 @@
  * SOFTWARE.
  */
 
-require_once("vendor/autoload.php");
+require_once("../../vendor/autoload.php");
 
 // This example health check function will always succeed
 //
@@ -45,7 +45,7 @@ function myHealthCheck2()
 
 // You can quickly itterate over all your health check functions
 // by adding them to a key => value array
-// 
+//
 // Array entries can either be the name of your health check function
 // as shown for check01 and check02 or an annonymous internal
 // function as shown for check03
